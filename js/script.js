242d9c1bb412fb2bfefb4c53c1e1a1cb64dc4f19
Raftor74 <raftor74@mail.ru>
2017-03-10 17:43:39 +0500

    Доработана функция MINIMAX, исправлены проверки на выйгрыш.

--- js/script.js
+++ js/script.js
@@ -5,6 +5,7 @@
 //Инициализация
 $(document).ready(function() {
     //Если выбран белый квадрат
+    //Устанавливаем цвет для компьютера и игрока
     $(".dots").click(function() {
         $(".guys, p").css("visibility", "hidden");
         $("td").css("visibility", "visible");
@@ -13,6 +14,7 @@ $(document).ready(function() {
         huCo = "white";
     });
     //Если выбран черный квадрат
+    //Устанавливаем цвет для компьютера и игрока
     $(".dots2").click(function() {
         $(".guys, p").css("visibility", "hidden");
         $("td").css("visibility", "visible");
@@ -24,6 +26,7 @@ $(document).ready(function() {
     $("td").click(function() {
         move(this, huPlayer, huCo);
     });
+
 });
 
 //Глобальные переменные
@@ -33,6 +36,7 @@ var huPlayer = "P";
 var aiPlayer = "C";
 //Глубина рекурсии
 var iter = 0;
+//Кол-во раундов
 var round = 0;
 var aiCo = "white";
 var huCo = "#333";
@@ -104,8 +108,13 @@ function reset() {
 
 //Минимакс функция
 function minimax(reboard, player) {
+    //Глубина рекурсии
     iter++;
+    //Создаём массив пустых клеток для перемещения
     let array = avail(reboard);
+    //Проверяем на конечные состояния
+    //Если позиция конечная, то устанавливаем ей очки.
+    //+10 если выигрывает AI, -10 если выигрывает человек, 0 при патовой ситуации
     if (winning(reboard, huPlayer)) {
         return {
             score: -10
@@ -120,14 +129,21 @@ function minimax(reboard, player) {
         };
     }
 
+    //Создаём массив перемещений
     var moves = [];
+    //Проходим по всем свободным клеткам
     for (var i = 0; i < array.length; i++) {
+        //Создаем объект "перемещение"
         var move = {};
+        //Устанавливаем ему свойство index со значением пустого поля.
         move.index = reboard[array[i]];
         reboard[array[i]] = player;
 
+        //Если ход игрока - запускаем Минимакс для компьютера
+        //Иначе - для человека
         if (player == aiPlayer) {
             var g = minimax(reboard, huPlayer);
+            //Сохраняем очки хода
             move.score = g.score;
         } else {
             var g = minimax(reboard, aiPlayer);
@@ -137,6 +153,7 @@ function minimax(reboard, player) {
         moves.push(move);
     }
 
+    //Находим лучший ход для человека и компьютера
     var bestMove;
     if (player === aiPlayer) {
         var bestScore = -10000;
@@ -163,44 +180,21 @@ function avail(reboard) {
     return reboard.filter(s => s != huPlayer && s != aiPlayer);
 }
 
-//Проверяет выйгрышные комбинации на поле
-/*function winning(board, player) {
-    if (
-        (board[0] == player && board[1] == player && board[2] == player) ||
-        (board[3] == player && board[4] == player && board[5] == player) ||
-        (board[6] == player && board[7] == player && board[8] == player) ||
-        (board[0] == player && board[3] == player && board[6] == player) ||
-        (board[1] == player && board[4] == player && board[7] == player) ||
-        (board[2] == player && board[5] == player && board[8] == player) ||
-        (board[0] == player && board[4] == player && board[8] == player) ||
-        (board[2] == player && board[4] == player && board[6] == player)
-    ) {
-        return true;
-    } else {
-        return false;
-    }
-}*/
-
-//Проверяет выйгрышные комбинации на поле
+//Проверяет выигрышные комбинации на поле
 function winning(board, player) {
-    if (
-        horizontalWin(board,player) ||
-        verticalWin(board,player)   ||
-        diagonalWin(board,player)
-    ) {
+    if (horizontalWin(board,player) || verticalWin(board,player) || diagonalWin(board,player)) {
         return true;
     } else {
         return false;
     }
 }
 
-//Проверяет выйгрышные позиции по горизонтали
+//Проверяет выигрышные позиции по горизонтали
 function horizontalWin(board,player){
     var findWinPos = false;
     for(var i=0; i<boardSize;i++){
-        for(var j=0; j<boardSize; j++){
-            console.log("horizontalWin i:"+i+" j:"+j);
-            if(board[i][j] == player){
+        for(var j=i*boardSize; j<boardSize*(i+1); j++){
+            if(board[j] == player){
                 findWinPos = true;
             } else {
                 findWinPos = false;
@@ -214,29 +208,28 @@ function horizontalWin(board,player){
     return false;
 }
 
-//Проверяет выйгрышные позиции по вертикали
+//Проверяет выигрышные позиции по вертикали
 function verticalWin(board,player){
     var findWinPos = false;
-    for(i=0; i<boardSize; i++){
-        for(j=i; j<boardSize; j+=boardSize){
-            if(board[i][j] == player){
+    for(var i=0; i<boardSize; i++){
+        for(var j=i; j<boardSize*boardSize; j+=boardSize){
+            if(board[j] == player){
                 findWinPos = true;
             } else {
                 findWinPos = false;
                 break;
             }
         }
-
         if(findWinPos)
             return true;
     }
     return false;
 }
 
-//Проверяет выйгрышные позиции по диагоналям
+//Проверяет выигрышные позиции по диагоналям
 function diagonalWin(board,player){
     var findWinPos = false;
-    for(var i=0; i<boardSize; i+=boardSize+1){
+    for(var i=0; i<boardSize*boardSize; i+=boardSize+1){
         if(board[i] == player){
             findWinPos = true;
         } else {
@@ -248,6 +241,7 @@ function diagonalWin(board,player){
     if(findWinPos)
         return true;
 
+    findWinPos = false;
     for(var i=boardSize-1; i<boardSize*boardSize-1; i+=boardSize-1){
         if(board[i] == player){
             findWinPos = true;
